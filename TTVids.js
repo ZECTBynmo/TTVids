21402880a794b986c5d2339c5da5ce05720bc7f2
Mike Vegeto <ZECTBynmo@.(none)>
2012-05-04 23:58:56 -0400

    First code commit

---
+++ TTVids.js
@@ -0,0 +1,7 @@
+javascript: (function () {
+	// Declare VidPlayer Class
+    var VidPlayer = function () {
+		var self = this;
+		
+	} // end VidPlayer()
+})();
---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(function(){var%20s=document.createElement('script');s.setAttribute('src','http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js');document.getElementsByTagName('body')[0].appendChild(s);})();
